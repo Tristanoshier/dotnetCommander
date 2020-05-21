58575a7d836414919e84b13c65d2cd7d73189be6
Tristanoshier <tsoshier@bsu.edu>
2020-05-21 15:05:52 -0400

    set up model and interface

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,22 @@
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+           var commands = new List<Command>
+           {
+               new Command{Id=0, HowTo="Boil an egg", Line="Boil Water", Platform="Kettle and Pan"},
+               new Command{Id=1, HowTo="Cut bread", Line="get a knife", Platform="Knife and chopping board"},
+               new Command{Id=2, HowTo="make tea", Line="place teabag in cup", Platform="kettle and cup"}
+           };
+
+           return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil Water", Platform="Kettle and Pan"};
+        }
+    } 
+}
