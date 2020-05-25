fc334846fa70cca351ad03f94c764098d470e418
Tristanoshier <tsoshier@bsu.edu>
2020-05-25 12:15:07 -0400

    add AutoMapper to Controller

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Commander.Models;
 using System.Collections.Generic;
 using Commander.Data;
+using AutoMapper;
+using Commander.Dtos;
 
 namespace Commander.Controllers 
 {
@@ -10,10 +12,12 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         //GET api/commands
@@ -27,12 +31,12 @@ namespace Commander.Controllers
 
         //GET api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
             if(commandItem != null)
             {
-                return Ok(commandItem);
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
             } 
             return NotFound();
         }
